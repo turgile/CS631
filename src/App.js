75ba5f1af30b5326879cd224d453e13f12cd908d
Eugene <eugeneturgil@gmail.com>
2018-04-29 23:13:26 -0400

    created the exercise page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { Component } from "react";
-import Amplify, { API } from "aws-amplify";
-import { withAuthenticator } from "aws-amplify-react";
-import Navigation from "./navbar";
-import Input from "./UI/Input";
-import { Form } from "react-bootstrap";
+import React, { Component } from 'react';
+import Amplify, { API } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import Navigation from './navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Exercise from './Exercise';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 Amplify.configure({
-  Auth: {
-    // REQUIRED - Amazon Cognito Identity Pool ID
-    identityPoolId: "us-east-2:e0ccd5c2-810f-476e-8070-db8a5bfac194",
-    // REQUIRED - Amazon Cognito Region
-    region: "us-east-2",
-    // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: "us-east-2_MPNhAtWNT",
-    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: "34jtv1h75oqar27ka2pc2q86ra",
-    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-    mandatorySignIn: true
-  } /*,
+	Auth: {
+		// REQUIRED - Amazon Cognito Identity Pool ID
+		identityPoolId: 'us-east-2:e0ccd5c2-810f-476e-8070-db8a5bfac194',
+		// REQUIRED - Amazon Cognito Region
+		region: 'us-east-2',
+		// OPTIONAL - Amazon Cognito User Pool ID
+		userPoolId: 'us-east-2_MPNhAtWNT',
+		// OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+		userPoolWebClientId: '34jtv1h75oqar27ka2pc2q86ra',
+		// OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+		mandatorySignIn: true,
+	} /*,
   API: {
     endpoints: [
       {
@@ -25,13 +27,13 @@ Amplify.configure({
         endpoint: "https://o4rzqchnta.execute-api.us-east-1.amazonaws.com/beta"
       }
     ]
-  }*/
+  }*/,
 });
 
 //console.log(Auth.currentUserInfo());
 
 class App extends Component {
-  /*componentDidMount() {
+	/*componentDidMount() {
     let apiName = "ServerlessQueryApi";
     let path = "/other";
     let myInit = {
@@ -47,37 +49,27 @@ class App extends Component {
         console.log(error);
       });
   }*/
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <Form>
-          <Input
-            label="Test"
-            validationCallback={function(value) {
-              if (value.length > 5) {
-                return "error";
-              } else {
-                return null;
-              }
-            }}
-            help="Enter help text here."
-          />
-          <Input
-            label="Test"
-            validationCallback={function(value) {
-              if (value.length > 5) {
-                return "error";
-              } else {
-                return null;
-              }
-            }}
-            help="Enter help text here."
-          />
-        </Form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Router>
+					<Grid>
+						<Row>
+							<Col>
+								<Navigation />
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<Route exact path="/" component={Home} />
+								<Route path="/exercise-management" component={Exercise} />
+							</Col>
+						</Row>
+					</Grid>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default withAuthenticator(App);
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,39 +1,33 @@
-import React from "react";
-import {
-  Navbar,
-  NavDropdown,
-  Nav,
-  NavItem,
-  MenuItem,
-  Glyphicon
-} from "react-bootstrap";
-import Auth from "aws-amplify";
+import React from 'react';
+import { Navbar, NavDropdown, Nav, NavItem, MenuItem, Glyphicon } from 'react-bootstrap';
+import Auth from 'aws-amplify';
+import { Link } from 'react-router-dom';
 
 const navigation = props => {
-  return (
-    <Navbar inverse collapseOnSelect staticTop fixedTop>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#brand">CS631 Final Project</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            Human Resources
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Class Management
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Registration
-          </NavItem>
-        </Nav>
-        <Nav pullRight />
-      </Navbar.Collapse>
-    </Navbar>
-  );
+	return (
+		<Navbar inverse collapseOnSelect staticTop fixedTop>
+			<Navbar.Header>
+				<Navbar.Brand>
+					<Link to="/">CS631 Final Project</Link>
+				</Navbar.Brand>
+				<Navbar.Toggle />
+			</Navbar.Header>
+			<Navbar.Collapse>
+				<Nav>
+					<NavItem eventKey={1} href="/human-resources">
+						Human Resources
+					</NavItem>
+					<NavItem eventKey={2} href="/class-management">
+						Class
+					</NavItem>
+					<NavItem eventKey={3} href="/exercise-management">
+						Exercise
+					</NavItem>
+				</Nav>
+				<Nav pullRight />
+			</Navbar.Collapse>
+		</Navbar>
+	);
 };
 
 export default navigation;
