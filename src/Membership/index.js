ca526d4e87adc9ea873c860fcb713437ea51af85
Eugene <eugeneturgil@gmail.com>
2018-05-04 19:16:44 -0400

    added membership

---
+++ src/Membership/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Authorization from '../HOC/Authorization';
+import FieldGroup from "../UI/FieldGroup"
+
+class Membership extends Component {
+    state = { show: false };
+
+    componentDidMount() {
+
+    }
+    render() {
+        return ()
+    }
+}
